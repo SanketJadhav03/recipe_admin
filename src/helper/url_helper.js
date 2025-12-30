e6562385196e088ad573cf046042e43a11144216
akash <akash2005kadam>
2025-12-30 20:21:29 +0530

    fix api error

--- src/helper/url_helper.js
+++ src/helper/url_helper.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://chefio-api.vercel.app/api";
-export const IMG_API_URL = "http://chefio-api.vercel.app/";
+export const API_URL = "https://chefio-api.vercel.app/api";
+export const IMG_API_URL = "https://chefio-api.vercel.app/";
